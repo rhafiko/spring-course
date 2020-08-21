eebcd005c017be870fd5f68d422bd4182f15ffe5
Najib El Alam <rhafiko@gmail.com>
2020-08-20 23:17:26 -0300

    created RequestRepository and RequestStageRepository

--- src/main/java/com/springcourse/repository/UserRepository.java
+++ src/main/java/com/springcourse/repository/UserRepository.java
@@ -11,6 +11,8 @@ import com.springcourse.domain.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    @Query("SELECT FROM User WHERE email = ?1 AND password = ?2")
+   
+	@Query("SELECT u FROM user u WHERE u.email = ?1 and u.password = ?2")
     public Optional<User> login(String email, String password);
+    
 }
