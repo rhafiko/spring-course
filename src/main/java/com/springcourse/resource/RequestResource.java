875dbbc7e13ec48447bd162326897f6f745171f8
Najib El Alam <rhafiko@gmail.com>
2020-10-04 19:58:53 -0300

    add default values for pages size and total pages.

--- src/main/java/com/springcourse/resource/RequestResource.java
+++ src/main/java/com/springcourse/resource/RequestResource.java
@@ -49,8 +49,8 @@ public class RequestResource {
 	}	
 	
 	@GetMapping
-	public ResponseEntity<PageModel<Request>> listAll (@RequestParam(value = "page") int page,
-			@RequestParam(value = "size") int size){
+	public ResponseEntity<PageModel<Request>> listAll (@RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "size", defaultValue = "10") int size){
 		
 		PageRequestModel pr = new PageRequestModel(page, size);
 		PageModel<Request> pm = requestService.listAllOnLazyModel(pr);
@@ -59,8 +59,8 @@ public class RequestResource {
 
 	@GetMapping("/{id}/request-stages")
 	public ResponseEntity<PageModel<RequestStage>> listAllByStagesId (@PathVariable(name = "id") Long requestId, 
-			@RequestParam(value = "page") int page,
-			@RequestParam(value = "size") int size){
+			@RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "size", defaultValue = "10") int size){
 
 		PageRequestModel pr = new PageRequestModel(page, size);
 		PageModel<RequestStage> pm = requestStageService.listAllByRequestIdOnLazyModel(requestId, pr);
--- src/main/java/com/springcourse/resource/UserResource.java
+++ src/main/java/com/springcourse/resource/UserResource.java
@@ -50,8 +50,8 @@ public class UserResource {
 	}
 	
 	@GetMapping
-	public ResponseEntity<PageModel<User>> listAll (@RequestParam(value = "page") int page,
-			@RequestParam(value = "size") int size){
+	public ResponseEntity<PageModel<User>> listAll (@RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "size", defaultValue = "10") int size){
 		
 		PageRequestModel pr = new PageRequestModel(page, size);
 		PageModel<User> pm = userService.listAllOnLazyModel(pr);
@@ -67,8 +67,8 @@ public class UserResource {
 	
 	@GetMapping("/{id}/requests")
 	public ResponseEntity<PageModel<Request>> listAllRequestsById (@PathVariable(name = "id") Long id, 
-			@RequestParam(value = "page") int page,
-			@RequestParam(value = "size") int size){
+			@RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "size", defaultValue = "10") int size){
 		
 		PageRequestModel pr = new PageRequestModel(page, size);
 		PageModel<Request> pm = requestService.listAllByOwnerIdOnLazyModel(id, pr);
