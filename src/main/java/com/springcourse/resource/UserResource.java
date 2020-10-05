78285be1753a0630400d23d02151ecb366a02df8
Najib El Alam <rhafiko@gmail.com>
2020-10-05 09:14:04 -0300

    improved user auth process using jwt and spring security.

--- src/main/java/com/springcourse/resource/UserResource.java
+++ src/main/java/com/springcourse/resource/UserResource.java
@@ -1,5 +1,8 @@
 package com.springcourse.resource;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +30,7 @@ import com.springcourse.dto.UserUpdateDto;
 import com.springcourse.dto.UserUpdateRoleDto;
 import com.springcourse.model.PageModel;
 import com.springcourse.model.PageRequestModel;
+import com.springcourse.security.JwtManager;
 import com.springcourse.service.RequestService;
 import com.springcourse.service.UserService;
 
@@ -37,6 +41,7 @@ public class UserResource {
 	@Autowired private UserService userService;
 	@Autowired private RequestService requestService;
 	@Autowired private AuthenticationManager authManager;
+	@Autowired private JwtManager jwtManager;
 	
 	
 	@PostMapping
@@ -69,12 +74,23 @@ public class UserResource {
 	}
 	
 	@PostMapping("/login")
-	public ResponseEntity<User> login (@RequestBody @Valid UserLoginDto user){
+	public ResponseEntity<String> login (@RequestBody @Valid UserLoginDto user){
 		
 		UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());
 		Authentication auth  = authManager.authenticate(token);
 		SecurityContextHolder.getContext().setAuthentication(auth);
-		return ResponseEntity.ok(null);
+		
+		org.springframework.security.core.userdetails.User userSpring =
+				(org.springframework.security.core.userdetails.User) auth.getPrincipal();
+		
+		String email = userSpring.getUsername();
+		List<String> roles = userSpring.getAuthorities()
+										.stream()
+										.map(authority -> authority.getAuthority())
+										.collect(Collectors.toList());
+		
+		String jwt = jwtManager.createToken(email, roles);
+		return ResponseEntity.ok(jwt);
 	}
 	
 	@GetMapping("/{id}/requests")
