59024b62ea062e6218255d031a4fa531c27624fb
Najib El Alam <rhafiko@gmail.com>
2020-10-04 21:52:31 -0300

    created new dto for proper validation handler.

---
+++ src/main/java/com/springcourse/dto/UserSaveDto.java
@@ -0,0 +1,41 @@
+package com.springcourse.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.springcourse.domain.Request;
+import com.springcourse.domain.RequestStage;
+import com.springcourse.domain.User;
+import com.springcourse.domain.enums.Role;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class UserSaveDto {
+	
+	@NotBlank(message = "name required")
+	private String name;
+	
+	@Email(message = "invalid email address")
+	private String email;
+	
+	@Size(min = 7, max = 99, message = "password must be between 7 and 99 lenght")
+	private String password;
+	
+	@NotNull
+	private Role role;
+	
+	private List<Request> requests = new ArrayList<Request>();
+	private List<RequestStage> stages = new ArrayList<RequestStage>();
+	
+	public  User transformToUser() {
+		User user = new User(null, this.name, this.email, this.password, this.role, this.requests, this.stages);
+		return user;		
+	}
+}
